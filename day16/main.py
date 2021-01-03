ee9ee969cfdf29d13407a7d850ece71559c32c24
NT-Coursera <tigerman21@hotmail.com>
2021-01-03 15:52:54 +0100

    correction typo name files

--- day16/main.py
+++ day16/main.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python
 
+def parse_allow_range(category_list):
+    all_range_per_category = {}
+    for line in category_list:
+        cat, allow_range = line.split(":")
+        allow_range = allow_range.split("or")
+        down1, up1 = allow_range[0].split("-")
+        down2, up2 = allow_range[1].split("-")
+        if cat not in all_range_per_category:
+            all_range_per_category[cat] = [(int(down1), int(up1)), (int(down2), int(up2))]
+        else:
+            print(f"category {cat} present 2 times")
+    return all_range_per_category
+
+
+def check_tickets(near_tickets, allow_range):
+    wrong_values = []
+    for ticket in near_tickets:
+        all_values = [int(val) for val in ticket.split(",")]
+        for value in all_values:
+            wrong = True
+            for k, v in allow_range:
+                for down, up in v:
+                    if down < value < up:
+                        print("ok")
+            if wrong:
+                wrong_values.append(value)
+    return wrong_values
+
+
 if __name__ == "__main__":
-    with open("input_nerby_tickets") as a:
+    with open("input_nearby_tickets") as a:
         near_ticket = a.read().split("\n")
     a.close()
     with open("input_your_ticket") as a:
@@ -14,3 +43,6 @@ if __name__ == "__main__":
     print(your_ticket)
     print(categories)
 
+    range_of_category = parse_allow_range(categories)
+
+    check_tickets(near_ticket, range_of_category)
